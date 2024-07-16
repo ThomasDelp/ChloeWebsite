21e6343c4066121d12561e171b1d259850437b86
ThomasDelp <84234345+ThomasDelp@users.noreply.github.com>
2024-07-16 21:32:55 +0200

    up background

--- js/index.js
+++ js/index.js
@@ -13,12 +13,21 @@ document.addEventListener("scroll", () => {
     var a = window.scrollY;
     var b = navbar.offsetHeight;
     currentScrollTop = a;
-   
+
     if (c < currentScrollTop && a > b + b) {
       navbar.classList.add("scrollUp");
     } else if (c > currentScrollTop && !(a <= b)) {
       navbar.classList.remove("scrollUp");
     }
+
+    if (a != 0){
+      console.log("1")
+      navbar.classList.add("isNotTop");
+    }else{
+      console.log("2")
+      navbar.classList.remove("isNotTop");
+    }
+
     c = currentScrollTop;
    
    console.log(a);
